0fd7568fc392d2d93fbe2ec27c77f35d49986b46
Aniket Mishra <137167377+Aniket-18-Mishra@users.noreply.github.com>
2026-06-11 22:41:21 +0530

    Add files via upload

---
+++ Day-10/Pattern4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            
+            char ch = 'A';
+            for (int k = 1; k <= i; k++) {
+                System.out.print(ch++);
+            }
+            
+            ch -= 2;
+            for (int l = i - 1; l >= 1; l--) {
+                System.out.print(ch--);
+            }
+            
+            System.out.println();
+        }
+        
+        scanner.close();
+    }
+}
